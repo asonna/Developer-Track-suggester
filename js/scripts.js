26920f772adeb015a3d90fd57126e1054930650b
Annie Sonna <nsab42@yahoo.com>
2016-10-21 11:14:57 -0700

    added some scripts and styles as well. git status q

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
+// business logic
+var whichDev = function(whichOption) {
+  if (whichOption === "A") {
+    return "#frontEnd";
+  } else if (whichOption === "B") {
+    return "#backEnd";
+  } else {
+    return "#backEnd"
+  }
+}
 
+
+// front end logic
+$(document).ready(function() {
+  $("#devOption").submit(function() {
+    event.preventDefault();
+    var devOptionSelection = $("#whichOption").val();
+    var result = whichDev(devOptionSelection);
+    $("#frontEnd").hide();
+    $("#backEnd").hide();
+    $("#both").hide();
+    $(result).show();
+  });
+});
