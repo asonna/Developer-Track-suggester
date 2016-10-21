a47f2d208ef1eba1d823388ab45f730b3ecbc2ef
Annie Sonna <nsab42@yahoo.com>
2016-10-21 13:47:45 -0700

    Added next level form html and scripts but not yet executing- need to debug now

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,63 @@
 var whichDev = function(whichOption) {
   if (whichOption === "A") {
     return "#frontEnd";
+
   } else if (whichOption === "B") {
     return "#backEnd";
+
   } else {
-    return "#backEnd"
+    return "#frontEnd", "#backEnd";
   }
 }
+
+// var whichLanguage = function(userInput1, userInput2, userInput3) {
+//   var cSh = 0;
+//   var jav = 0;
+//   var php = 0;
+//   var rub = 0;
+//
+//   if (userInput1="cSharp") {
+//     cSch += 1;
+//   } else if (userInput1="java") {
+//     jav = +=1;
+//   } else if (userInput1="php") {
+//     php = +=1;
+//   } else {
+//     rub = +1;
+//   }
+//
+//   if (userInput2="cSharp") {
+//     cSch += 1;
+//   } else if (userInput2="java") {
+//     jav = +=1;
+//   } else if (userInput2="php") {
+//     php = +=1;
+//   } else {
+//     rub = +1;
+//   }
+//
+//   if (userInput3="cSharp") {
+//     cSch += 1;
+//   } else if (userInput3="java") {
+//     jav = +=1;
+//   } else if (userInput3="php") {
+//     php = +=1;
+//   } else {
+//     rub = +1;
+//   }
+//
+//   if (cSch >= 2) {
+//     return "cSharp";
+//   } else if (jav >= 2) {
+//     return "java";
+//   } else if (php >= 2) {
+//     return "php";
+//   } else if (rub >= 2) {
+//     return "ruby"
+//   } else {
+//     return "NONE"
+//   }
+// }
 
 
 // front end logic
@@ -19,6 +70,18 @@ $(document).ready(function() {
     $("#frontEnd").hide();
     $("#backEnd").hide();
     $("#both").hide();
+    $("#devOption").hide();
     $(result).show();
   });
+
+  // $("#backEnd").submit(function() {
+  //   event.preventDefault();
+  //   var userInput1 = $("#question2").val();
+  //   var userInput1 = $("#question3").val();
+  //   var userInput1 = $("#question4").val();
+  //
+  //   var result = whichLanguage(userInput1, userInput2, userInput3);
+  //
+  //   $(".language").text(result);
+  // });
 });
